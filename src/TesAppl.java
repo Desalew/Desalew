fad6d81fd29d7add71f65e9c927be88b7e93da5a
Desalew <80298941+Desalew@users.noreply.github.com>
2021-04-08 20:45:09 -0400

    Update TesAppl.java

--- src/TesAppl.java
+++ src/TesAppl.java
@@ -1,5 +1,6 @@
 public class TesAppl {
     public static void main(String[] args) {
         System.out.println( "Hello World Java Code");
+        System.out.println( "Hello World Java Code");
     }
 }
